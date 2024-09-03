066ec40e69a9c93d0bfa056a42668b23003d6a66
ashafaatadhis <skullreaper387@gmail.com>
2024-09-04 06:17:32 +0700

    feat: fixing relation some in playlist

--- src/services/playlistServices.ts
+++ src/services/playlistServices.ts
@@ -33,6 +33,7 @@ export const getAllByUserId = async (
                   genreId: genre,
                   subGenreId: subgenre,
                 },
+                deletedAt: null,
               },
             },
           }
